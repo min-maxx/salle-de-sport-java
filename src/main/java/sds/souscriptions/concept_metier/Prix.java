ea55dc9046bd26e284cfd7c106f929917b0bd81f
maxx4web <maximepelletier45@gmail.com>
2019-07-01 17:08:46 +0200

    Correction inspections

--- src/main/java/sds/souscriptions/concept_metier/Prix.java
+++ src/main/java/sds/souscriptions/concept_metier/Prix.java
@@ -3,6 +3,10 @@ package sds.souscriptions.concept_metier;
 import java.util.Objects;
 
 public class Prix {
+
+    private static final int NB_CHIFFRE_APRES_VIRGULE_EN_EURO = 2;
+    private static final double CENT = Math.pow(10, NB_CHIFFRE_APRES_VIRGULE_EN_EURO);
+
     public static Prix de(double valeur) {
         return new Prix(valeur);
     }
@@ -13,13 +17,13 @@ public class Prix {
         this.valeur = valeur;
     }
 
-    public Prix applique(TauxReduction taux) {
+    Prix applique(TauxReduction taux) {
         double prixReduit = this.valeur - (this.valeur) * taux.valeur / 100;
-        return Prix.de(arrondi(prixReduit, 2));
+        return Prix.de(arrondi(prixReduit));
     }
 
-    private double arrondi(double nombre, int nbChiffreApresVirgule) {
-        return (double) ((int) (nombre * Math.pow(10, nbChiffreApresVirgule) + .5)) / Math.pow(10, nbChiffreApresVirgule);
+    private double arrondi(double nombre) {
+        return (double) ((int) (nombre * CENT + .5)) / CENT;
     }
 
     @Override
